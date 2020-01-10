5b489eebf68577aeeefc1fd533db24c306517f0f
Jose_Develaw <jose@develaw.com>
2020-01-10 19:48:10 +0100

    User Interface - Filter Activity

--- app/src/main/java/com/example/knowyogrow/Login.java
+++ app/src/main/java/com/example/knowyogrow/Login.java
@@ -2,7 +2,10 @@ package com.example.knowyogrow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button enter = (Button) findViewById(R.id.buttonEnter);
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(Login.this, Filter.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
