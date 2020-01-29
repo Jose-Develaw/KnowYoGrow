8d81cd67ccb1194e51071a269c37f3c2e15de69c
Jose_Develaw <jose@develaw.com>
2020-01-29 23:34:46 +0100

    FavButton functionalitites improved, progressbar centered, detail strings fixed, other minor visual improvements

--- app/src/main/java/com/example/knowyogrow/Detail.java
+++ app/src/main/java/com/example/knowyogrow/Detail.java
@@ -3,7 +3,9 @@ package com.example.knowyogrow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.database.Cursor;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,22 +28,36 @@ public class Detail extends AppCompatActivity {
     TextView descriptionText;
     StrainComplete sc;
     DBInterface dbInterface;
+    boolean already = false;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        Intent intent = getIntent();
+        sc = (StrainComplete) intent.getSerializableExtra("strainClicked");
+        dbInterface = new DBInterface(Detail.this);
+        dbInterface.open();
+        Cursor c =  dbInterface.getStrainT();
+        if (c.moveToFirst() && c != null) {
+            do {
+                int id = c.getInt(0);
+                if (id == sc.getStrain().getId()) {already = true;}
+            } while (c.moveToNext());
+        }
+
+
 
 
 
         //Button toFav = findViewById(R.id.addFavButton);
-        FloatingActionButton floatingFav = findViewById(R.id.floatingFav);
+        final FloatingActionButton floatingFav = findViewById(R.id.floatingFav);
         floatingFav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                boolean already = false;
+
                 dbInterface = new DBInterface(Detail.this);
                 dbInterface.open();
                 Cursor c =  dbInterface.getStrainT();
@@ -53,24 +69,40 @@ public class Detail extends AppCompatActivity {
                 }
 
                 if (!already) {
+                    already = true;
                     ArrayList<String> flavors = (ArrayList<String>) sc.getStrain().getFlavors();
                     ArrayList<String> positive_fx = (ArrayList<String>) sc.getStrain().getEffects().getPositive();
                     ArrayList<String> negative_fx = (ArrayList<String>) sc.getStrain().getEffects().getNegative();
                     ArrayList<String> medicos_fx = (ArrayList<String>) sc.getStrain().getEffects().getMedical();
+                    floatingFav.setImageDrawable(getDrawable(android.R.drawable.star_big_on));
+                    floatingFav.setBackgroundTintList(ColorStateList.valueOf(getColor(R.color.colorPrimary)));
 
                     dbInterface.newFavourite(sc.getStrain().getId(), sc.getName(), sc.getStrain().getRace(), flavors, positive_fx, negative_fx, medicos_fx);
                     Toast.makeText(Detail.this, "Added to favourites", Toast.LENGTH_LONG).show();
 
                 } else {
-
-                    Toast.makeText(Detail.this, "This strain is already among your favs", Toast.LENGTH_LONG).show();
+                    already = false;
+                    floatingFav.setImageDrawable(getDrawable(android.R.drawable.star_big_off));
+                    floatingFav.setBackgroundTintList(ColorStateList.valueOf(getColor(android.R.color.white)));
+                    dbInterface.deleteFavourite(sc.getStrain().getId());
+                    Toast.makeText(getLayoutInflater().getContext(), "Strain '"+sc.getName()+"' has been removed from favs",
+                            Toast.LENGTH_LONG).show();
                 }
 
 
             }
         });
-        Intent i = getIntent();
-        sc = (StrainComplete) i.getSerializableExtra("strainClicked");
+
+        if(!already) {floatingFav.setCompatElevation(8);
+         floatingFav.setImageDrawable(getDrawable(android.R.drawable.star_big_off));
+        floatingFav.setBackgroundTintList(ColorStateList.valueOf(getColor(android.R.color.white)));} else {
+
+            floatingFav.setImageDrawable(getDrawable(android.R.drawable.star_big_on));
+            floatingFav.setBackgroundTintList(ColorStateList.valueOf(getColor(R.color.colorPrimary)));
+        }
+
+
+
         descriptionText= findViewById(R.id.descriptionText);
         Call<Description> descriptionCall = ApiService.getApiService().getDescription(sc.getStrain().getId());
         descriptionCall.enqueue(new Callback<Description>() {
@@ -105,10 +137,40 @@ public class Detail extends AppCompatActivity {
         name.setText(sc.getName().toUpperCase());
         String strainRace = sc.getStrain().getRace();
         race.setText(strainRace.toUpperCase());
-        positiveText.setText(sc.getStrain().getEffects().getPositive().toString());
-        negativeText.setText(sc.getStrain().getEffects().getNegative().toString());
+
+
+        String poslist ="";
+        for (int i = 0; i < sc.getStrain().getEffects().getPositive().size() -1 ; i++) {
+            poslist += sc.getStrain().getEffects().getPositive().get(i) +", ";
+        }
+        poslist += sc.getStrain().getEffects().getPositive().get(sc.getStrain().getEffects().getPositive().size() -1);
+        positiveText.setText(poslist);
+
+        String neglist ="";
+        for (int i = 0; i < sc.getStrain().getEffects().getNegative().size() -1 ; i++) {
+            neglist += sc.getStrain().getEffects().getNegative().get(i) +", ";
+        }
+        neglist += sc.getStrain().getEffects().getNegative().get(sc.getStrain().getEffects().getNegative().size() -1);
+        negativeText.setText(neglist);
+
+        String medlist ="";
+        for (int i = 0; i < sc.getStrain().getEffects().getMedical().size() -1 ; i++) {
+            medlist += sc.getStrain().getEffects().getMedical().get(i) +", ";
+        }
+        medlist += sc.getStrain().getEffects().getMedical().get(sc.getStrain().getEffects().getMedical().size() -1);
+        medicalText.setText(medlist);
+
+        String flavors ="";
+        for (int i = 0; i < sc.getStrain().getFlavors().size() -1 ; i++) {
+            flavors += sc.getStrain().getFlavors().get(i) +", ";
+        }
+        flavors += sc.getStrain().getFlavors().get(sc.getStrain().getFlavors().size() -1);
+        flavorText.setText(flavors);
+
+
+        /*negativeText.setText(sc.getStrain().getEffects().getNegative().toString());
         medicalText.setText(sc.getStrain().getEffects().getMedical().toString());
-        flavorText.setText(sc.getStrain().getFlavors().toString());
+        flavorText.setText(sc.getStrain().getFlavors().toString());*/
 
         ImageView imageDetail = (ImageView) findViewById(R.id.imageDetail);
         switch (strainRace) {
